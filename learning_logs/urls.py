7d9073162087a47f95f2558f0469d5e214358f34
Chiprian Tutueanu <53701761+chip00@users.noreply.github.com>
2019-10-17 19:48:06 +0300

    Add files via upload

---
+++ learning_logs/urls.py
@@ -0,0 +1,26 @@
+#Defines URL patterns for learning_logs.
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    #Home page
+    path('', views.index, name='index'),
+
+    #Show all topics.
+    path('topics/', views.topics, name='topics'),
+
+    #detail page for one topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    #page for adding topics
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    #page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    #page for editing existing entries
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+]
---
+++ learning_logs/views.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
+def index(request):
+    #The home page for learning logs
+    return render(request, 'learning_logs/index.html')
+
+@login_required
+def topics(request):
+    #show topics.
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+@login_required
+def topic(request, topic_id):
+    #Show a single topic with its entries.
+    topic = Topic.objects.get(id=topic_id)
+    #make sure the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404
+
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+
+    return render(request, 'learning_logs/topic.html', context)
+
+@login_required
+def new_topic(request):
+    #add new topic
+    if request.method != 'POST':
+        #no data has been submitted.create a blank form
+        form = TopicForm()
+    else:
+        #POST data submitted
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+
+    return render(request, 'learning_logs/new_topic.html', context)
+
+@login_required
+def new_entry(request, topic_id):
+    #add a new entry for a topic
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #no data has been submitted,create a blank form.
+        form = EntryForm()
+    else:
+        form = EntryForm(data= request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit= False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                                args=[topic_id]))
+
+    context = {'topic': topic, 'form': form }
+
+    return render(request, 'learning_logs/new_entry.html', context)
+
+@login_required
+def edit_entry(request, entry_id):
+    #edit the existing entries
+
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
+
+    if request.method != 'POST':
+        #Get request,fill the form with the existing entry
+        form = EntryForm(instance=entry)
+
+    else:
+        #POST data submitted,process entry
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                                args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
